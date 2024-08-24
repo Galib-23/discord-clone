1336cd11152540d906ecb4c3d1857ad70c0292d8
Galib-23 <aagalib2323@gmail.com>
2024-08-24 22:37:57 +0600

    setup and configure site theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       This is a protected route
+    <ModeToggle />
     </div>
   )
 }
