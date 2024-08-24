c2554a2d6e45df2e3660219e34275e6076a30073
Galib-23 <aagalib2323@gmail.com>
2024-08-24 21:47:03 +0600

    setup and configure clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className='bg-black'>
+    <div className='h-full flex items-center justify-center'>
       {children}
     </div>
   )
