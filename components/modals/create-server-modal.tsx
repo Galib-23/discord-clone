0fd7866d53de0cbe2b01b4c9f0203c2783de6e1b
Galib-23 <aagalib2323@gmail.com>
2024-08-29 02:16:02 +0600

    create user modal store and server modal

---
+++ components/modals/create-server-modal.tsx
@@ -0,0 +1,123 @@
+"use client"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import FileUpload from "../file-upload"
+import axios from 'axios';
+import { useRouter } from "next/navigation"
+import { useModal } from "@/hooks/use-modal-store"
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required"
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is requierd"
+  })
+})
+
+const CreateServerModal = () => {
+  const { isOpen, onClose, type } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "createServer";
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+  const isLoading = form.formState.isSubmitting;
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      axios.post("/api/servers", values);
+      form.reset();
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  }
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden px-6 pt-8">
+        <DialogHeader className="">
+          <DialogTitle className="text-2xl text-center font-bold">Create your server</DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="flex items-center justify-center text-center">
+              <FormField
+                control={form.control}
+                name="imageUrl"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <FileUpload
+                        endpoint="serverImage"
+                        value={field.value}
+                        onChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="uppercase font-bold text-xs text-zinc-500 dark:text-secondary/70">
+                    Server Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input disabled={isLoading} className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" placeholder="Enter Server Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="py-4">
+              <Button variant="primary" disabled={isLoading} type="submit">Create</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+
+  )
+}
+
+export default CreateServerModal
