a9624f144683be61567e6e85f51489f8648de822
Galib-23 <aagalib2323@gmail.com>
2024-08-29 01:40:07 +0600

    complete navigation item component

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,9 @@ import { db } from '@/lib/db';
 import { redirect } from 'next/navigation';
 import React from 'react'
 import NavigationAction from './navigation-action';
+import { Separator } from '@/components/ui/separator';
+import { ScrollArea } from '../ui/scroll-area';
+import NavigationItem from './navigation-item';
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -23,6 +26,18 @@ const NavigationSidebar = async () => {
   return (
     <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22]'>
       <NavigationAction />
+      <Separator
+        className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto'
+      />
+      <ScrollArea className='flex-1 w-full'>
+        {
+          servers.map((server) => (
+            <div key={server.id} className='mb-4'>
+              <NavigationItem id={server.id} name={server.name} imageUrl={server.imageUrl} />
+            </div>
+          ))
+        }
+      </ScrollArea>
     </div>
   )
 }
