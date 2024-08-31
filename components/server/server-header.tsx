aba54c939d9ec607ba95f8cee2c9bf2ca16deec3
Galib-23 <aagalib2323@gmail.com>
2024-08-31 23:28:15 +0600

    fetch server sidebar info

---
+++ components/server/server-header.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const ServerHeader = () => {
+  return (
+    <div>ServerHeader</div>
+  )
+}
+
+export default ServerHeader
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,15 +1,7 @@
 import { currentProfile } from "@/lib/current-profile"
+import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
-
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-
 
 interface ServerSidebarProps {
   serverId: string
@@ -22,23 +14,36 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
     return redirect('/');
   }
 
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc"
+        }
+      },
+      members: {
+        include: {
+          profile: true
+        },
+        orderBy: {
+          role: "asc"
+        }
+      }
+    }
+  });
+
+  const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT);
+  const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO);
+  const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO);
+  const members = server?.members.filter((member) => member.profileId !== profile.id);
+
+  const role = server?.members.find((member) => member.profileId === profile?.id)?.role;
+
   return (
-    <div>
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">Home</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
 
     </div>
   )
