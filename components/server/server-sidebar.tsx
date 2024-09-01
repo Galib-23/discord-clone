ee94cdb1573cb3a16b417e85d494cb8bdb1fe0c3
Galib-23 <aagalib2323@gmail.com>
2024-09-01 16:17:49 +0600

    complete server header dropdown functionality

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
+import ServerHeader from "./server-header";
 
 interface ServerSidebarProps {
   serverId: string
@@ -42,9 +43,13 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
 
   const role = server?.members.find((member) => member.profileId === profile?.id)?.role;
 
+  if (!server) {
+    return redirect('/');     
+  }
+
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
-
+      <ServerHeader server={server} role={role} />
     </div>
   )
 }
