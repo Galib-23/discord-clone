be8321957868ce2085f5df6051b5bf4ad6c2ead5
Galib-23 <aagalib2323@gmail.com>
2024-09-08 00:52:48 +0600

    create leave server route

--- components/modals/leave-server-modal.tsx
+++ components/modals/leave-server-modal.tsx
@@ -3,44 +3,34 @@
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog"
 import { useModal } from "@/hooks/use-modal-store"
-import { Label } from "../ui/label";
-import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { Check, Copy, RefreshCw } from "lucide-react";
-import { useOrigin } from "@/hooks/use-origin";
 import { useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 
 
 const LeaveServerModal = () => {
-  const [copied, setCopied] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const { isOpen, onOpen, onClose, type, data } = useModal();
-  const origin = useOrigin();
+  const { isOpen, onClose, type, data } = useModal();
   const { server } = data;
   const isModalOpen = isOpen && type === "leaveServer";
+  const router = useRouter();
 
-  const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
-
-  const onCopy = () => {
-    navigator.clipboard.writeText(inviteUrl);
-    setCopied(true);
-
-    setTimeout(() => {
-      setCopied(false);
-    }, 1000)
-  }
-
-  const onNew = async () => {
+  const onClick = async () => {
     try {
       setIsLoading(true);
-      const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);
-      onOpen("invite", { server: response.data })
+      await axios.patch(`/api/servers/${server?.id}/leave`);
+
+      onClose();
+      router.refresh();
+      router.push("/");
     } catch (error) {
       console.log(error);
     } finally {
@@ -52,33 +42,34 @@ const LeaveServerModal = () => {
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden px-6 pt-8">
         <DialogHeader className="">
-          <DialogTitle className="text-2xl text-center font-bold">Invite friends</DialogTitle>
+          <DialogTitle className="text-2xl text-center font-bold">
+            Leave Server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Are you sure you want to leave
+            <span
+              className="font-semibold text-indigo-500"
+            >{server?.name}</span>
+          </DialogDescription>
         </DialogHeader>
-        <div className="p-6">
-          <Label className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-            Server Invite Link
-          </Label>
-          <div className="flex items-center mt-2 gap-x-2">
-            <Input
+        <DialogFooter className="bg-gray-100 px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button
               disabled={isLoading}
-              className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-              value={inviteUrl}
-            />
-            <Button disabled={isLoading} onClick={onCopy} size="icon">
-              {copied ? <Check className="w-4 h-4" /> : <Copy className="w-4 h-4" />}
+              onClick={onClose}
+              variant="ghost"
+            >
+              Cancel
+            </Button>
+            <Button
+              disabled={isLoading}
+              variant="primary"
+              onClick={onClick}
+            >
+              Confirm
             </Button>
           </div>
-          <Button
-            onClick={onNew}
-            disabled={isLoading}
-            variant="link"
-            size="sm"
-            className="text-xs text-zinc-500 mt-4"
-          >
-            Generate New Link
-            <RefreshCw className="h-4 w-4 ml-2" />
-          </Button>
-        </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
 
