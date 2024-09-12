96264abd4a2d101bd80d683b046ace92696b8c37
Galib-23 <aagalib2323@gmail.com>
2024-09-13 01:11:14 +0600

    create mobile toggle mode

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import ChatHeader from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const ChannelIdPage = () => {
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  }
+}
+
+const ChannelIdPage = async ({
+  params
+}: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId
+    }
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id
+    }
+  })
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
   return (
-    <div>ChannelIdPage</div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader 
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
   )
 }
 
