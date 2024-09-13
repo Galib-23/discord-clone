02798edaddb39b66b68d613ae6dfc78e1ac9cb6b
Galib-23 <aagalib2323@gmail.com>
2024-09-14 03:09:32 +0600

    create chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
@@ -37,10 +38,20 @@ const ChannelIdPage = async ({
 
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-      <ChatHeader 
+      <ChatHeader
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   )
---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,25 @@
+
+import { db } from "./db";
+import { redirect } from "next/navigation";
+import { NextApiRequest } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+
+export const currentProfile = async (req: NextApiRequest) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return null;
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  if (!profile) {
+    redirect('/')
+  }
+
+  return profile;
+};
