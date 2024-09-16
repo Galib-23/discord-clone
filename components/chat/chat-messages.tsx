de4bfe2d78312cde82f4836dd132a4634121d746
Galib-23 <aagalib2323@gmail.com>
2024-09-16 23:48:32 +0600

    create chat sockets

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import ChatItem from "./chat-item";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 interface ChatMessagesProps {
   name: string;
@@ -39,7 +40,11 @@ const ChatMessages = ({
   paramValue,
   type,
 }: ChatMessagesProps) => {
+
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
+
   const {
     data,
     fetchNextPage,
@@ -52,6 +57,8 @@ const ChatMessages = ({
     paramKey,
     paramValue
   });
+
+  useChatSocket({queryKey, addKey, updateKey});
 
   if (status === "pending") {
     return (
